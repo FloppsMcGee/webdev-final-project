5f732afdc63e22899e27deb0e3cbc1457921cb27
Muhammed Ahmed <muhammed@pdx.edu>
2026-06-09 13:58:09 -0700

    remove scrollbar and adjust some colors

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ const menuToggle = document.querySelector(".menu-toggle");
 const navLinks = document.querySelector(".nav-links");
 const contactForm = document.querySelector("#contact-form");
 const formStatus = document.querySelector("#form-status");
-const garage = document.querySelector(".garage");
 
 if ("scrollRestoration" in history) {
   history.scrollRestoration = "manual";
@@ -34,6 +33,8 @@ contactForm.addEventListener("submit", (event) => {
 
   contactForm.reset();
 });
+
+const garage = document.querySelector(".garage");
 
 if (garage) {
   const garageObserver = new IntersectionObserver(
